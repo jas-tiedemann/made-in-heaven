4e508d92cc9edb902da4d706467e439be4200adb
Jasper Tiedemann <jas.tiedemann@ok.de>
2022-11-09 17:32:23 +0100

    creates header component

---
+++ src/components/header/header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './header.css';
+import headerImage from "../../assets/images/header-image.webp"
+
+const Header = () => {
+  return (
+    <div className="header__main">
+        <div className="header__image">
+            <img src={headerImage} alt="header__image"></img>
+        </div>
+          <nav className="header__nav">
+            <ul className="header__list">
+              <li className="header__list__item">
+                <a className="header__list__item__link" href="#">Start</a>
+              </li>
+              <li className="header__list__item">
+                <a className="header__list__item__link" href="#">Öffnungszeiten</a>
+              </li>
+              <li className="header__list__item">
+                <a className="header__list__item__link" href="#">Tattoo-Service</a>
+              </li>
+              <li className="header__list__item">
+                <a className="header__list__item__link" href="#">Piercing-Service</a>
+              </li>
+              <li className="header__list__item">
+                <a className="header__list__item__link" href="#">Kontakt</a>
+              </li>
+            </ul>
+          </nav>
+    </div>
+  )
+}
+
+export default Header;
