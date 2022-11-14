1337017fe46f155c3e0609cc00d8b60ec963e698
Jasper Tiedemann <jas.tiedemann@ok.de>
2022-11-14 15:56:30 +0100

    changes styling of header component, reworks some colors

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import './header.css';
-import headerImage from "../../assets/images/header-image.webp"
+import headerImage from "../../assets/images/logo.png";
 
 const Header = () => {
   return (
     <div className="header__main">
-        <div className="header__image">
-            <img src={headerImage} alt="header__image"></img>
+        <div className="header__image__wrapper">
+            <img src={headerImage} className="header__image" alt="header__image"></img>
         </div>
-          <nav className="header__nav">
-            <ul className="header__list">
-              <li className="header__list__item">
-                <a className="header__list__item__link" href="#">Start</a>
-              </li>
-              <li className="header__list__item">
-                <a className="header__list__item__link" href="#">Öffnungszeiten</a>
-              </li>
-              <li className="header__list__item">
-                <a className="header__list__item__link" href="#">Tattoo-Service</a>
-              </li>
-              <li className="header__list__item">
-                <a className="header__list__item__link" href="#">Piercing-Service</a>
-              </li>
-              <li className="header__list__item">
-                <a className="header__list__item__link" href="#">Kontakt</a>
-              </li>
-            </ul>
-          </nav>
+        <nav className="header__nav">
+          <ul className="header__list">
+            <li className="header__list__item">
+              <a className="header__list__item__link" href="#">Start</a>
+            </li>
+            <li className="header__list__item">
+              <a className="header__list__item__link" href="#">Öffnungszeiten</a>
+            </li>
+            <li className="header__list__item">
+              <a className="header__list__item__link" href="#">Tattoo-Service</a>
+            </li>
+            <li className="header__list__item">
+              <a className="header__list__item__link" href="#">Piercing-Service</a>
+            </li>
+            <li className="header__list__item">
+              <a className="header__list__item__link" href="#">Kontakt</a>
+            </li>
+          </ul>
+        </nav>
     </div>
   )
 }
