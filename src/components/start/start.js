595b37badfaea6474b3392ac1dd8037b94a2817d
jas-tiedemann <jas.tiedemann@ok.de>
2023-02-27 12:40:01 +0100

    puts welcome section into start component

--- src/components/start/start.js
+++ src/components/start/start.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import './start.scss';
+import React from "react";
+import "./start.scss";
+import "../../styles/config.scss";
 
 const Start = () => {
   return (
-    <div className="start__image">
+    <div className="start">
+      <div className="start__willkommen">
+            <h1 className="start__header header--large standard--width">WILLKOMMEN BEI MADE IN HEAVEN</h1>
+            <button className="start__button --btn-large"><a className="start__anchor anchor--small" href="#tattoos">Mehr erfahren</a></button>
+        </div>
     </div>
-  )
+  );
 }
 
 export default Start;
--- src/components/zeiten/zeiten.js
+++ src/components/zeiten/zeiten.js
@@ -4,21 +4,17 @@ import "./zeiten.scss";
 const Zeiten = () => {
   return (
     <div className="zeiten" id="oeffnungszeiten">
-        <div className="zeiten__willkommen">
-            <h1 className="zeiten__header header--large">WILLKOMMEN BEI MADE IN HEAVEN</h1>
-            <button className="zeiten__button --btn-large"><a className="zeiten__anchor anchor--small" href="#tattoos">Mehr erfahren</a></button>
-        </div>
-        <div className="zeiten__info">
+        <div className="zeiten__info standard--width">
             <div className="zeiten__bgimg">
                 <h2 className="zeiten__header header--medium">Öffnungszeiten</h2>
             </div>
             <div className="zeiten__list">
-                <h3 className="header--small">Meldet euch!</h3>
+                <p className="header--small">Meldet euch!</p>
                 <div className="zeiten__wrapper">
                     <p className="zeiten__time text--medium">Mo. - Fr.: 12:00 - 17:00 Uhr</p>
                     <p className="zeiten__time text--medium">Sa.: 12:00 - 14:00 Uhr</p>
                 </div>
-                <p className="text--small">Nach Vereinbarung auch Termine vor der regulären Öffnungszeit möglich.</p>
+                <p className="zeiten__time text--small">Nach Vereinbarung auch Termine vor der regulären Öffnungszeit möglich.</p>
             </div>
         </div>
     </div>
