041075373308f49c4b9823ea4ab75efecdff8678
jas-tiedemann <jas.tiedemann@ok.de>
2023-02-27 12:39:10 +0100

    adds content to piercing component

--- src/components/piercing/piercing.js
+++ src/components/piercing/piercing.js
@@ -1,9 +1,27 @@
 import React from "react";
+import "./piercing.scss";
+import "../../styles/config.scss";
+import imgPiercing from "../../assets/images/piercing-ohr.jpg";
 
 const Piercing = () => {
   return (
-    <div>piercing</div>
-  )
+    <section className="piercing standard--center" id="piercings">
+      <div className="piercing__header header--medium standard--width">
+        <h2 className="header--medium">DAS BESTE PIERCING</h2>
+      </div>
+      <div className="piercing__row standard--width">
+        <div className="piercing__text">
+          <h3 className="header--small">KOMM IN DEN PIERCINGHIMMEL</h3>
+          <p className="text--medium">
+            Wir denken an dich! Bei uns bekommst du die wahrscheinlich ehrlichste Piercingberatung, die man sich nur wünschen kann. Uns geht es nicht nur darum DIR deinen Wunsch zu erfüllen, wir werden dir auch ehrlich von Piercings abraten, wenn sie zu Gefährlich sind, deine Anatomie nicht passend ist oder wenn du unsere Meinung wissen möchtest, ob dir das Piercing steht oder nicht. 
+            Wir klären DICH zu 100% auf, damit du gut vorbeireitet bist, dich gut um dein Piercing zu kümmern. Du darfst uns auch immer mit deinen Fragen löchern. 
+            Wir lassen dich nicht im Stich! Du kannst uns bei Notfällen oder auch bei kleineren Fragen immer erreichen. 
+          </p>
+        </div>
+        <img src={imgPiercing} alt="img-piercing" className="piercing--image"></img>
+      </div>
+    </section>
+  );
 }
 
-export default Piercing
+export default Piercing;
