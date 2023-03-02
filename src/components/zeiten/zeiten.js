1ba839f311a1d562f8e7f1e138232d05dafc6037
jas-tiedemann <jas.tiedemann@ok.de>
2023-03-02 18:23:54 +0100

    changes styling of zeiten component

--- src/components/zeiten/zeiten.js
+++ src/components/zeiten/zeiten.js
@@ -4,7 +4,7 @@ import "./zeiten.scss";
 const Zeiten = () => {
   return (
     <div className="zeiten" id="oeffnungszeiten">
-        <div className="zeiten__info standard--width">
+        <div className="zeiten__info standard--width info-wrapper rounded-cor">
             <div className="zeiten__bgimg">
                 <h2 className="zeiten__header header--medium">Öffnungszeiten</h2>
             </div>
