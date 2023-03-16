063aa3c32f288a125bdf204a09c79c9ae08bc44b
jas-tiedemann <jas.tiedemann@ok.de>
2023-03-16 16:45:06 +0100

    fills start comp with more content

--- src/components/start/start.js
+++ src/components/start/start.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./start.scss";
 import "../../styles/config.scss";
+import { AiOutlineArrowDown } from "react-icons/ai";
+
 
 
 const Start = () => {
   return (
     <div className="start">
-      <div className="start__willkommen">
-            <h1 className="start__header header header--large standard--width">WILLKOMMEN BEI MADE IN HEAVEN</h1>
-            <button className="start__button --btn-large"><a className="start__anchor anchor--small" href="#tattoos">Mehr erfahren</a></button>
+      <div className="start__willkommen small--width">
+            <h1 className="header--large">Willkommen bei Made in Heaven</h1>
+            <p className="text--large">Das Made in Heaven ist ein professionelles Tattoo- und Piercingstudio. Insgesamt sind wir 6 Mädels, die für euch alles geben. Wir sind immer bemüht unsere Kunden glücklich zu machen und arbeiten mit den höchsten Standards, um dies zu gewährleisten.</p>
+            <div className="start__button-termin rounded-cor">
+              <a className="book-appoi" href="#tattoos">Termin buchen</a>
+            </div>
         </div>
+        <button className="start__button-scroll">
+          <a className="start__arrow" href="#tattoos">
+            <AiOutlineArrowDown className="icon--large arrow-scroll" />
+          </a>
+        </button>
     </div>
   );
 }
