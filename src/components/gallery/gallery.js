fb852225b99edd768148a9beaee01878d2f83a6c
jas-tiedemann <jas.tiedemann@ok.de>
2023-03-17 14:14:30 +0100

    implements react-image-gallery package

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,32 +1,32 @@
 import React from "react";
 import "./gallery.scss";
+import ImageGallery from "react-image-gallery";
 import img00 from "../../assets/images/tattoo-schnecke.jpg";
 import img01 from "../../assets/images/tattoo-blumen.jpg";
 import img02 from "../../assets/images/tattoo-schädel.jpg";
 import img03 from "../../assets/images/tattoo-waschbär.jpg";
 import img04 from "../../assets/images/tattoo-blumen-01.jpg";
 import img05 from "../../assets/images/tattoo-hirsch.jpg";
-import img06 from "../../assets/images/tattoo-bein.jpg";
-import img07 from "../../assets/images/tattoo-arm.jpg";
-import img08 from "../../assets/images/tattoo-adler.jpg";
+import img06 from "../../assets/images/piercing-ohr.jpg";
 
 const Gallery = () => {
-    const images = [[img00, "schnecke"], [img01, "schädel"], [img02, "waschbär"], [img03, "blumen00"], [img04, "blumen01"], [img05, "hirsch"], [img06, "arm"], [img07, "bein"], [img08, "adler"]]
+
+    const images = [
+        {original: img00, thumbnail: img00},
+        {original: img01, thumbnail: img01},
+        {original: img02, thumbnail: img02},
+        {original: img03, thumbnail: img03},
+        {original: img04, thumbnail: img04},
+        {original: img05, thumbnail: img05},
+        {original: img06, thumbnail: img06}
+    ];
 
     return (
-        <section className="gallery" id="tattoos">
+        <section className="gallery" id="gallery-comp">
             <div className="gallery__header-wrapper standard--center">
                 <h3 className="header--medium">Galerie</h3>
             </div>
-            <div className="gallery__wrapper">
-                {images.map((x) => {
-                    return (
-                        <div className="gallery__artist ">
-                            <img src={x[0]} alt={x[1]} className="gallery__img"></img>
-                        </div>
-                    );
-                })}
-            </div>
+            <ImageGallery className="gallery__slider" items={images} />
         </section>
     );
 }
