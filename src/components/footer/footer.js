1a04171c69a3e081e391fb2b05fa3570199f4f43
jas-tiedemann <jas.tiedemann@ok.de>
2023-03-27 14:19:35 +0200

    adds content to footer comp

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,22 @@
 import React from "react";
+import "./footer.scss";
 
 const Footer = () => {
   return (
-    <section>
-        <p>Impressum</p>
-        <p>Kontakt</p>
+    <section className="footer standard--center">
+      <div className="footer__imprint">
+        <ul className="footer__list">
+          <li>Made in Heaven</li>
+          <li>Katharina-Sophie Pieper</li>
+          <li>Dielingerstr 42a</li>
+          <li>49074 Osnabrück</li>
+          <li>Einzelunternehmer</li>
+          <li>madeinheaven.pt@outlook.de</li>
+        </ul>
+      </div>
+      <p className="footer__text text--medium">Telefon
+        <a className="footer__anchor text--medium" href="tel:01573 6824997">: 01573 6824997</a>
+      </p>
     </section>
   );
 }
