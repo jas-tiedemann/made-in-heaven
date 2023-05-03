96a464a1020c6687c70829d82dc6b90b974573d5
jas-tiedemann <jas.tiedemann@ok.de>
2023-05-03 14:03:24 +0200

    makes footer comoponent responsive

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,22 +1,59 @@
-import React from "react";
-import "./footer.scss";
+import React, { useState } from "react";
+import arrowUp from "../../assets/vectors/arrow-up.svg";
+import arrowDown from "../../assets/vectors/arrow-down.svg";
 
 const Footer = () => {
+
+  const [extended, setExtended] = useState(false);
+  
+  const handleExtend = () => {
+    setExtended(true);
+  }
+
+  const handleCollapse = () => {
+    setExtended(false);
+  }
+
+  const displayButton = () => {
+    if (extended) {
+      return (
+        <button onClick={handleCollapse} className="footer__button text">
+          <span className="text">Impressum</span>
+          <img src={arrowUp} alt="arrow-up" className="footer__icon icon--medium"></img>
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={handleExtend} className="footer__button text">
+          <span className="text">Impressum</span>
+          <img src={arrowDown} alt="arrow-down" className="footer__icon icon--medium"></img>
+        </button>
+      );
+    }
+  }
+
   return (
     <section className="footer standard--center">
-      <div className="footer__imprint">
-        <ul className="footer__list">
-          <li>Made in Heaven</li>
-          <li>Katharina-Sophie Pieper</li>
-          <li>Dielingerstr 42a</li>
-          <li>49074 Osnabrück</li>
-          <li>Einzelunternehmer</li>
-          <li>madeinheaven.pt@outlook.de</li>
-        </ul>
+      <div className="footer__phone">
+        <p className="footer__text text">Telefon
+          <a className="footer__anchor text" href="tel:01573 6824997">: 01573 6824997</a>
+        </p>
       </div>
-      <p className="footer__text text--medium">Telefon
-        <a className="footer__anchor text--medium" href="tel:01573 6824997">: 01573 6824997</a>
-      </p>
+      <div className="footer__imprint-wrapper">
+        <div className="footer__collapse">
+          {displayButton()}
+        </div>
+        <div className={extended ? "footer__imprint" : "footer__imprint_hidden"}>
+          <ul className="footer__list">
+            <li>Made in Heaven</li>
+            <li>Katharina-Sophie Pieper</li>
+            <li>Dielingerstraße 42a</li>
+            <li>49074 Osnabrück</li>
+            <li>Einzelunternehmer</li>
+            <li><a className="footer__anchor" href="mailto:madeinheaven.pt@outlook.de">madeinheaven.pt@outlook.de</a></li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
