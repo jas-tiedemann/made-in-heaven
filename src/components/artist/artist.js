e0a6fba981105f01c486bd106d0142a66c5b6da2
jas-tiedemann <jas.tiedemann@ok.de>
2023-05-12 14:23:26 +0200

    reduces code redundancy, styles the component

--- src/components/artist/artist.js
+++ src/components/artist/artist.js
@@ -1,11 +1,5 @@
 import React, { useState } from "react";
-import img00 from "../../assets/images/tattoo-schnecke.webp";
-import img01 from "../../assets/images/tattoo-blumen.webp";
-import img02 from "../../assets/images/tattoo-schädel.webp";
-import img03 from "../../assets/images/tattoo-waschbär.webp";
-import img04 from "../../assets/images/tattoo-blumen-01.webp";
-import img05 from "../../assets/images/tattoo-hirsch.webp";
-import img06 from "../../assets/images/piercing-ohr.webp";
+import close from "../../assets/vectors/close.svg";
 
 const Artist = ( {name, imgPerson, pronouns, instagram, position, imgArray} ) => {
 
@@ -22,26 +16,26 @@ const Artist = ( {name, imgPerson, pronouns, instagram, position, imgArray} ) =>
   const displayExtended = () => {
       setExtended(true);
   }
-  
+
   const displayCollapsed = () => {
       setExtended(false);
   }
 
   const collapsedComp = (
     <div className="artist-wrapper_collapsed">
-      <a className="artist__link" onClick={displayExtended}>
+      <a className="artist__link" href="#artist" onClick={displayExtended}>
         <img className="artist__portrait_collapsed" src={imgPerson} alt="person-img"></img>
-        <p className="artist__text artist__text_collapsed text--large">{name}</p>
+        <p className="artist__text artist__text_collapsed  header--small">{name}</p>
       </a>
     </div>
   );
 
   const extendedComp = (
-    <div className="artist-wrapper_extended rounded-cor">
-      <button onClick={displayCollapsed}>Close</button>
+    <div className="artist-wrapper_extended">
+      <button className="artist__button rounded-cor" onClick={displayCollapsed}><img className="icon--large" src={close} alt="close-button"></img></button>
       <div className="artist__info-wrapper">
-        <div className="artist__image-wrapper">
-          <img className="artist__portrait_extended" src={imgPerson} alt="person-img"></img>
+        <div className="artist__portrait-wrapper">
+          <img className="artist__portrait_extended rounded-cor" src={imgPerson} alt="person-img"></img>
         </div>
         <div className="artist__text-wrapper">
           <h3 className="artist__text artist__text_extended">{name}</h3>
@@ -50,26 +44,21 @@ const Artist = ( {name, imgPerson, pronouns, instagram, position, imgArray} ) =>
           <a className="artist__text artist__text_extended"  href={instagram}>{"Instagram: "+ instagram}</a>
         </div>
       </div>
-      <div className="artist__gallery-wrapper">
-        <img src={img00} alt="" className="artist__image"></img>
-        <img src={img01} alt="" className="artist__image"></img>
-        <img src={img02} alt="" className="artist__image"></img>
-        <img src={img03} alt="" className="artist__image"></img>
-        <img src={img04} alt="" className="artist__image"></img>
-        <img src={img05} alt="" className="artist__image"></img>
-        <img src={img06} alt="" className="artist__image"></img>
-        <img src={img06} alt="" className="artist__image"></img>
-        <img src={img06} alt="" className="artist__image"></img>
-        <img src={img06} alt="" className="artist__image"></img>
+      <div className="artist__image-wrapper">
+        {imgArray.map(x => {
+          return (
+            <img src={x} alt="" className="artist__image"></img>
+          );
+        })}
       </div>
     </div>
   );
 
   return(
-    <article className="artist">
-      {displayComp(extendedComp, collapsedComp)}  
+    <article className="artist rounded-cor">
+      {displayComp(extendedComp, collapsedComp)}
     </article>
-  ); 
+  );
 }
 
 export default Artist;
