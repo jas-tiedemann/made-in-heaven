49490e00e4ed7ca08cf024f59b82790596d86001
jas-tiedemann <jas.tiedemann@ok.de>
2023-05-25 13:33:34 +0200

    corrects opening time

--- src/components/opening/opening.js
+++ src/components/opening/opening.js
@@ -7,7 +7,7 @@ const Opening = () => {
         <p className="header--small">Meldet euch!</p>
         <div className="opening__wrapper">
             <p className="opening__time text">Mo. - Fr.: 12:00 - 17:00 Uhr</p>
-            <p className="opening__time text">Sa.: 12:00 - 14:00 Uhr</p>
+            <p className="opening__time text">Sa.: 12:00 - 15:00 Uhr</p>
         </div>
         <p className="opening__time">Nach Vereinbarung auch Termine vor der regulären Öffnungszeit möglich</p>
     </section>
