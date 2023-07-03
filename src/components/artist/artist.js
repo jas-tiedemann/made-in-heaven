ea7b138b88a19ef10f6c443af9c49b758d4b3873
jas-tiedemann <jas.tiedemann@ok.de>
2023-07-03 12:04:22 +0200

    improves mobile styling for artist comp

--- src/components/artist/artist.js
+++ src/components/artist/artist.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import close from "../../assets/vectors/close.svg";
 
-const Artist = ( {name, imgPerson, pronouns, instagram, position, imgArray} ) => {
+const Artist = ( {name, imgPerson, pronouns, instaName, instaLink, position, imgArray} ) => {
 
   const [extended, setExtended] = useState(false);
 
@@ -25,23 +25,28 @@ const Artist = ( {name, imgPerson, pronouns, instagram, position, imgArray} ) =>
     <div className="artist-wrapper_collapsed">
       <a className="artist__link" href="#artist" onClick={displayExtended}>
         <img className="artist__portrait_collapsed" src={imgPerson} alt="person-img"></img>
-        <p className="artist__text artist__text_collapsed  header--medium">{name}</p>
+        <div className="artist__text-wrapper_collapsed">
+          <p className="artist__text artist__text_collapsed header--small">{name}</p>
+          <p className="artist__text artist__text_collapsed text--medium">{position}</p>
+        </div>
       </a>
     </div>
   );
 
   const extendedComp = (
     <div className="artist-wrapper_extended">
-      <button className="artist__button rounded-cor" onClick={displayCollapsed}><img className="icon--large" src={close} alt="close-button"></img></button>
+      <button className="artist__button rounded-cor" onClick={displayCollapsed}>
+        <img className="icon--large" src={close} alt="close-button"></img>
+      </button>
       <div className="artist__info-wrapper">
         <div className="artist__portrait-wrapper">
           <img className="artist__portrait_extended rounded-cor" src={imgPerson} alt="person-img"></img>
         </div>
-        <div className="artist__text-wrapper">
+        <div className="artist__text-wrapper_extended">
           <h3 className="artist__text artist__text_extended">{name}</h3>
           <p className="artist__text artist__text_extended">{pronouns}</p>
           <p className="artist__text artist__text_extended">{position}</p>
-          <a className="artist__text artist__text_extended"  href={instagram}>{"Instagram: "+ instagram}</a>
+          <a className="artist__text artist__text_extended"  name={instaName} href={instaLink} target="_blank" rel="noreferrer">{"Instagram: "+ instaName}</a>
         </div>
       </div>
       <div className="artist__image-wrapper">
