0669958063e9c777543f5d714daae0bff93e7c14
jas-tiedemann <jas.tiedemann@ok.de>
2023-07-12 13:05:35 +0200

    fixes mistakes in headers

--- src/pages/opening/opening.js
+++ src/pages/opening/opening.js
@@ -3,13 +3,13 @@ import React from "react";
 const Opening = () => {
   return (
     <section className="opening" id="opening-comp">
-        <h2 className="header header--medium">Öffnungszeiten</h2>
-        <p className="header header--small">Meldet euch!</p>
-        <div className="opening__wrapper">
-            <p className="opening__time text">Mo. - Fr.: 12:00 - 17:00 Uhr</p>
-            <p className="opening__time text">Sa.: 12:00 - 15:00 Uhr</p>
-        </div>
-        <p className="opening__time">Nach Vereinbarung auch Termine vor der regulären Öffnungszeit möglich</p>
+      <h2 className="header header--medium">Öffnungszeiten</h2>
+      <h3 className="header header--small">Meldet euch!</h3>
+      <div className="opening__wrapper">
+          <p className="opening__time text">Mo. - Fr.: 12:00 - 17:00 Uhr</p>
+          <p className="opening__time text">Sa.: 12:00 - 15:00 Uhr</p>
+      </div>
+      <p className="opening__time">Nach Vereinbarung auch Termine vor der regulären Öffnungszeit möglich</p>
     </section>
   );
 }
--- src/pages/service/service.js
+++ src/pages/service/service.js
@@ -8,7 +8,7 @@ const Service = () => {
       <div className="service-wrapper">
         <div className="service__text">
           <article className="service__article_piercing"> 
-          <h3 className="header--small">Kunst auf deinem Körper</h3>
+          <h3 className="header header--small">Kunst auf deinem Körper</h3>
           <p className="text">
             Lass uns dein individuelles Tattoo entwickeln. Du teilst uns deine Wünsche mit und wir schauen, was wir daraus machen können. Wir wissen, dass es in Osnabrück und Umgebung eine große Auswahl an Studios gibt. Aber Made in Heaven gibt es nur ein Mal! Wir freuen uns über dein Vertrauen und werden ein einzigartiges Tattoo für dich entwickeln.
           </p>
@@ -16,7 +16,7 @@ const Service = () => {
           
           <article className="service__article_tattoo">
             <div className="service__text_tattoo">
-              <h3 className="header--small">Komm in den Piercinghimmel</h3>
+              <h3 className="header header--small">Komm in den Piercinghimmel</h3>
               <p className="text">
                 Wir denken an dich! Bei uns bekommst du die wahrscheinlich ehrlichste Piercingberatung, die man sich nur wünschen kann. Uns geht es nicht nur darum DIR deinen Wunsch zu erfüllen, wir werden dir auch ehrlich von Piercings abraten, wenn sie zu gefährlich sind, deine Anatomie nicht passt oder wenn du unsere Meinung wissen möchtest, ob dir das Piercing steht oder nicht. Wir klären DICH zu 100% auf, damit du gut vorbereitet bist, dich gut um dein Piercing zu kümmern. Du darfst uns auch immer mit deinen Fragen löchern.
               </p>
