6d68f12b69771cffcd09fbe688bc795d312815d4
jas-tiedemann <jas.tiedemann@ok.de>
2023-10-04 12:02:18 +0200

    add missing images and restructuring of img files

--- src/pages/team/team.js
+++ src/pages/team/team.js
@@ -1,52 +1,144 @@
 import React from "react";
 import Artist from "../../components/artist/artist";
-import imgPerson00 from "../../assets/images/angi-img.webp";
-import imgPerson01 from "../../assets/images/ani-img.webp";
-import imgPerson02 from "../../assets/images/elise-img.webp";
-import imgPerson03 from "../../assets/images/elliot-img.webp";
-import imgPerson04 from "../../assets/images/kathi-img.webp";
-import imgPerson05 from "../../assets/images/toni-img.webp";
-import imgPerson06 from "../../assets/images/lydia-img.webp";
-import imgPerson07 from "../../assets/images/mareike-img.webp";
-import imgPerson08 from "../../assets/images/slim-img.webp";
-import img00 from "../../assets/images/tattoo-schnecke.webp";
-import img01 from "../../assets/images/tattoo-blumen.webp";
-import img02 from "../../assets/images/tattoo-schädel.webp";
-import img03 from "../../assets/images/tattoo-waschbär.webp";
-import img04 from "../../assets/images/tattoo-blumen-01.webp";
-import img05 from "../../assets/images/tattoo-hirsch.webp";
-import img06 from "../../assets/images/piercing-ohr.webp";
+import imgProfileAngi from "../../assets/images/artist-images/Angi/profile.webp";
+import imgProfileAni from "../../assets/images/artist-images/Ani/profile.webp";
+import imgProfileElise from "../../assets/images/artist-images/Elise/profile.webp";
+import imgProfileElliot from "../../assets/images/artist-images/Elliot/profile.webp";
+import imgProfileKathi from "../../assets/images/artist-images/Kathi/profile.webp";
+import imgProfileLydia from "../../assets/images/artist-images/Lydia/profile.webp";
+import imgProfileMareike from "../../assets/images/artist-images/Mareike/profile.webp";
+import imgProfileShino from "../../assets/images/artist-images/Shino/profile.jpeg";
+import imgProfileSlim from "../../assets/images/artist-images/Slim/profile.webp";
+import imgProfileToni from "../../assets/images/artist-images/Toni/profile.webp";
+
+import AngiImg00 from "../../assets/images/artist-images/Ani/00.jpg";
+import AngiImg01 from "../../assets/images/artist-images/Ani/01.jpg";
+import AngiImg02 from "../../assets/images/artist-images/Ani/02.jpg";
+import AngiImg03 from "../../assets/images/artist-images/Ani/03.jpg";
+import AngiImg04 from "../../assets/images/artist-images/Ani/04.jpg";
+import AngiImg05 from "../../assets/images/artist-images/Ani/05.jpg";
+import AngiImg06 from "../../assets/images/artist-images/Ani/06.jpg";
+import AngiImg07 from "../../assets/images/artist-images/Ani/07.jpg";
+import AngiImg08 from "../../assets/images/artist-images/Ani/08.jpg";
+
+import AniImg00 from "../../assets/images/artist-images/Ani/00.jpg";
+import AniImg01 from "../../assets/images/artist-images/Ani/01.jpg";
+import AniImg02 from "../../assets/images/artist-images/Ani/02.jpg";
+import AniImg03 from "../../assets/images/artist-images/Ani/03.jpg";
+import AniImg04 from "../../assets/images/artist-images/Ani/04.jpg";
+import AniImg05 from "../../assets/images/artist-images/Ani/05.jpg";
+import AniImg06 from "../../assets/images/artist-images/Ani/06.jpg";
+import AniImg07 from "../../assets/images/artist-images/Ani/07.jpg";
+import AniImg08 from "../../assets/images/artist-images/Ani/08.jpg";
+
+import EliseImg00 from "../../assets/images/artist-images/Elise/00.JPG";
+import EliseImg01 from "../../assets/images/artist-images/Elise/01.JPG";
+import EliseImg02 from "../../assets/images/artist-images/Elise/02.JPG";
+import EliseImg03 from "../../assets/images/artist-images/Elise/03.JPG";
+import EliseImg04 from "../../assets/images/artist-images/Elise/04.JPG";
+import EliseImg05 from "../../assets/images/artist-images/Elise/05.JPG";
+import EliseImg06 from "../../assets/images/artist-images/Elise/06.JPG";
+import EliseImg07 from "../../assets/images/artist-images/Elise/07.JPG";
+import EliseImg08 from "../../assets/images/artist-images/Elise/08.JPG";
+
+import ElliotImg00 from "../../assets/images/artist-images/Elliot/00.jpg";
+import ElliotImg01 from "../../assets/images/artist-images/Elliot/01.jpg";
+import ElliotImg02 from "../../assets/images/artist-images/Elliot/02.jpg";
+import ElliotImg03 from "../../assets/images/artist-images/Elliot/03.jpg";
+import ElliotImg04 from "../../assets/images/artist-images/Elliot/04.jpg";
+import ElliotImg05 from "../../assets/images/artist-images/Elliot/05.jpg";
+import ElliotImg06 from "../../assets/images/artist-images/Elliot/06.jpg";
+import ElliotImg07 from "../../assets/images/artist-images/Elliot/07.jpg";
+import ElliotImg08 from "../../assets/images/artist-images/Elliot/08.jpg";
+
+import KathiImg00 from "../../assets/images/artist-images/Kathi/00.JPG";
+import KathiImg01 from "../../assets/images/artist-images/Kathi/01.JPG";
+import KathiImg02 from "../../assets/images/artist-images/Kathi/02.JPG";
+import KathiImg03 from "../../assets/images/artist-images/Kathi/03.JPG";
+import KathiImg04 from "../../assets/images/artist-images/Kathi/04.JPG";
+import KathiImg05 from "../../assets/images/artist-images/Kathi/05.JPG";
+import KathiImg06 from "../../assets/images/artist-images/Kathi/06.jpg";
+import KathiImg07 from "../../assets/images/artist-images/Kathi/07.jpg";
+import KathiImg08 from "../../assets/images/artist-images/Kathi/08.jpg";
+import KathiImg09 from "../../assets/images/artist-images/Kathi/09.jpg";
+import KathiImg10 from "../../assets/images/artist-images/Kathi/10.jpg";
+
+import LydiaImg00 from "../../assets/images/artist-images/Lydia/00.jpg";
+import LydiaImg01 from "../../assets/images/artist-images/Lydia/01.jpg";
+import LydiaImg02 from "../../assets/images/artist-images/Lydia/02.jpg";
+import LydiaImg03 from "../../assets/images/artist-images/Lydia/03.jpg";
+import LydiaImg04 from "../../assets/images/artist-images/Lydia/04.jpg";
+import LydiaImg05 from "../../assets/images/artist-images/Lydia/05.jpg";
+import LydiaImg06 from "../../assets/images/artist-images/Lydia/06.jpg";
+import LydiaImg07 from "../../assets/images/artist-images/Lydia/07.jpg";
+import LydiaImg08 from "../../assets/images/artist-images/Lydia/08.jpg";
+
+import MareikeImg00 from "../../assets/images/artist-images/Mareike/00.jpg";
+import MareikeImg01 from "../../assets/images/artist-images/Mareike/01.jpg";
+import MareikeImg02 from "../../assets/images/artist-images/Mareike/02.jpg";
+import MareikeImg03 from "../../assets/images/artist-images/Mareike/03.jpg";
+import MareikeImg04 from "../../assets/images/artist-images/Mareike/04.PNG";
+import MareikeImg05 from "../../assets/images/artist-images/Mareike/05.PNG";
+import MareikeImg06 from "../../assets/images/artist-images/Mareike/06.jpg";
+import MareikeImg07 from "../../assets/images/artist-images/Mareike/07.jpg";
+import MareikeImg08 from "../../assets/images/artist-images/Mareike/08.jpg";
+
+import ShinoImg00 from "../../assets/images/artist-images/Shino/00.jpeg";
+import ShinoImg01 from "../../assets/images/artist-images/Shino/01.jpeg";
+import ShinoImg02 from "../../assets/images/artist-images/Shino/02.jpeg";
+import ShinoImg03 from "../../assets/images/artist-images/Shino/03.jpeg";
+import ShinoImg04 from "../../assets/images/artist-images/Shino/04.jpg";
+import ShinoImg05 from "../../assets/images/artist-images/Shino/05.jpeg";
+import ShinoImg06 from "../../assets/images/artist-images/Shino/06.jpg";
+
+import SlimImg00 from "../../assets/images/artist-images/Slim/00.jpg";
+import SlimImg01 from "../../assets/images/artist-images/Slim/01.jpg";
+import SlimImg02 from "../../assets/images/artist-images/Slim/02.jpg";
+import SlimImg03 from "../../assets/images/artist-images/Slim/03.jpg";
+import SlimImg04 from "../../assets/images/artist-images/Slim/04.jpg";
+import SlimImg05 from "../../assets/images/artist-images/Slim/05.jpg";
+import SlimImg06 from "../../assets/images/artist-images/Slim/06.jpg";
+import SlimImg07 from "../../assets/images/artist-images/Slim/07.jpg";
+
+import ToniImg00 from "../../assets/images/artist-images/Toni/00.jpg";
+import ToniImg01 from "../../assets/images/artist-images/Toni/01.jpg";
+import ToniImg02 from "../../assets/images/artist-images/Toni/02.jpg";
+import ToniImg03 from "../../assets/images/artist-images/Toni/03.jpg";
+import ToniImg04 from "../../assets/images/artist-images/Toni/04.jpg";
 
 
 const Team = () => {
 
     const teamInfo = [
-        {name: "Angi", imgPerson: imgPerson00, pronouns: "sie/ihr", instaName: "Angii", instaLink: "https://www.instagram.com/milu_tattoo_/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo, Azubi und Shopmanagerin", imgArray:
-            [img00, img01, img02, img03, img04, img05, img06]
+        {name: "Angi", imgPerson: imgProfileAngi, pronouns: "sie/ihr", instaName: "Angii", instaLink: "https://www.instagram.com/milu_tattoo_/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo, Azubi und Shopmanagerin", imgArray:
+            [AngiImg00, AngiImg01, AngiImg02, AngiImg03, AngiImg04, AngiImg05, AngiImg06, AngiImg07, AngiImg08]
         },
-        {name: "Ani", imgPerson: imgPerson01, pronouns: "sie/ihr", instaName: "Ani Levottomuus", instaLink: "https://www.instagram.com/delightfulsleep/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
-            [img00, img01, img02, img03, img04, img05, img06]
+        {name: "Ani", imgPerson: imgProfileAni, pronouns: "sie/ihr", instaName: "Ani Levottomuus", instaLink: "https://www.instagram.com/delightfulsleep/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
+            [AniImg00, AniImg01, AniImg02, AniImg03, AniImg04, AniImg05, AniImg06, AniImg07, AniImg08]
         },
-        {name: "Elise", imgPerson: imgPerson02, pronouns: "sie/ihr", instaName: "Elise", instaLink: "https://www.instagram.com/elisesam.tattoo/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
-            [img00, img01, img02, img03, img04, img05, img06]
+        {name: "Elise", imgPerson: imgProfileElise, pronouns: "sie/ihr", instaName: "Elise", instaLink: "https://www.instagram.com/elisesam.tattoo/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
+            [EliseImg00, EliseImg01, EliseImg02, EliseImg03, EliseImg04, EliseImg05, EliseImg06, EliseImg07, EliseImg08]
         },
-        {name: "Elliot", imgPerson: imgPerson03, pronouns: "er/ihm", instaName: "Elliot", instaLink: "https://www.instagram.com/eggs_tattoo/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
-            [img00, img01, img02, img03, img04, img05, img06]
+        {name: "Elliot", imgPerson: imgProfileElliot, pronouns: "er/ihm", instaName: "Elliot", instaLink: "https://www.instagram.com/eggs_tattoo/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
+            [ElliotImg00, ElliotImg01, ElliotImg02, ElliotImg03, ElliotImg04, ElliotImg05, ElliotImg06, ElliotImg07, ElliotImg08]
         },
-        {name: "Kathi", imgPerson: imgPerson04, pronouns: "sie/ihr", instaName: "Onkel Kathi", instaLink: "https://www.instagram.com/onka_tattoo/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo und Piercing", imgArray:
-            [img00, img01, img02, img03, img04, img05, img06]
+        {name: "Kathi", imgPerson: imgProfileKathi, pronouns: "sie/ihr", instaName: "Onkel Kathi", instaLink: "https://www.instagram.com/onka_tattoo/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo und Piercing", imgArray:
+            [KathiImg00, KathiImg01, KathiImg02, KathiImg03, KathiImg04, KathiImg05, KathiImg06, KathiImg07, KathiImg08, KathiImg09, KathiImg10]
         },
-        {name: "Toni", imgPerson: imgPerson05, pronouns: "sie/ihr", instaName: "tattos by toni", instaLink: "https://www.instagram.com/cosmaids.tintcan/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
-            [img00, img01, img02, img03, img04, img05, img06]
+        {name: "Toni", imgPerson: imgProfileToni, pronouns: "sie/ihr", instaName: "tattos by toni", instaLink: "https://www.instagram.com/cosmaids.tintcan/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
+            [ToniImg00, ToniImg01, ToniImg02, ToniImg03, ToniImg04]
         },
-        {name: "Lydia", imgPerson: imgPerson06, pronouns: "sie/ihr", instaName: "TattooStudio9", instaLink: "https://www.instagram.com/tattoostudio_9_/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
-            [img00, img01, img02, img03, img04, img05, img06]
+        {name: "Lydia", imgPerson: imgProfileLydia, pronouns: "sie/ihr", instaName: "TattooStudio9", instaLink: "https://www.instagram.com/tattoostudio_9_/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
+            [LydiaImg00, LydiaImg01, LydiaImg02, LydiaImg03, LydiaImg04, LydiaImg05, LydiaImg06, LydiaImg07, LydiaImg08]
         },
-        {name: "Mareike", imgPerson: imgPerson07, pronouns: "sie/ihr", instaName: "Mareike", instaLink: "https://www.instagram.com/graw.ttt/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
-        [img00, img01, img02, img03, img04, img05, img06]
+        {name: "Mareike", imgPerson: imgProfileMareike, pronouns: "sie/ihr", instaName: "Mareike", instaLink: "https://www.instagram.com/graw.ttt/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Tattoo", imgArray:
+            [MareikeImg00, MareikeImg01, MareikeImg02, MareikeImg03, MareikeImg04, MareikeImg05, MareikeImg06, MareikeImg07, MareikeImg08]
         },
-        {name: "Slim", imgPerson: imgPerson08, pronouns: "sie/ihnen", instaName: "Alisha/Slim", instaLink: "https://www.instagram.com/fit_for_a_piercing.pt/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Piercing", imgArray:
-            [img00, img01, img02, img03, img04, img05, img06]
+        {name: "Slim", imgPerson: imgProfileSlim, pronouns: "sie/ihnen", instaName: "Alisha/Slim", instaLink: "https://www.instagram.com/fit_for_a_piercing.pt/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Piercing", imgArray:
+            [SlimImg00, SlimImg01, SlimImg02, SlimImg03, SlimImg04, SlimImg05, SlimImg06, SlimImg07]
+        },
+        {name: "Shino", imgPerson: imgProfileShino, pronouns: "sie/ihnen", instaName: "Alisha/Slim", instaLink: "https://www.instagram.com/fit_for_a_piercing.pt/?igshid=Y2IzZGU1MTFhOQ%3D%3D", position: "Piercing", imgArray:
+            [ShinoImg00, ShinoImg01, ShinoImg02, ShinoImg03, ShinoImg04, ShinoImg05, ShinoImg06]
         }
     ];
 
