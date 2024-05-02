ec1139766dc37f79aee93f37219edd12b693c81c
jas-tiedemann <jas.tiedemann@ok.de>
2024-05-03 00:11:11 +0200

    Replaces imges in prices.js with three new ones

--- src/pages/prices/prices.js
+++ src/pages/prices/prices.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pricingImg00 from "../../assets/images/pricing-image00.jpg";
 import pricingImg01 from "../../assets/images/pricing-image01.jpg";
+import pricingImg02 from "../../assets/images/pricing-image02.jpg";
 
 const Prices = () => {
   return (
@@ -11,6 +12,7 @@ const Prices = () => {
         <div className="prices-image--wrapper">
             <img src={pricingImg00} alt="pricing-image00" className="prices-image prices-image--small"></img>
             <img src={pricingImg01} alt="pricing-image01" className="prices-image prices-image--big"></img>
+            <img src={pricingImg02} alt="pricing-image02" className="prices-image prices-image--big"></img>
         </div>
     </div>
   );
